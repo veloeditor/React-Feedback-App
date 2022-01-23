7c25de8761e6c1c6119aedd4b5a0f2eaf50840ea
Brian Wilson <wilsonedit@gmail.com>
2022-01-23 08:29:14 -0600

    switched up tab settings for prettier

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Header from './components/Header';
 
 function App() {
-    return (
-        <>
-            <Header />
-            <div className='container'>
-                <h1>My App</h1>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <div className='container'>
+        <h1>My App</h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
