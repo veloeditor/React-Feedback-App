4c0812491b7ecf83830f4dea95b86304f4e9599c
Brian Wilson <wilsonedit@gmail.com>
2022-01-29 12:09:12 -0600

    added validation to feedback form that is disabled to start until at least 10 characters are entered

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,8 +4,20 @@ import Button from './shared/Button';
 
 function FeedbackForm() {
   const [text, setText] = useState('');
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState('');
 
   const handleTextChange = (e) => {
+    if (text === '') {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== '' && text.trim().length <= 10) {
+      setBtnDisabled(true);
+      setMessage('Text must be at least 10 characters long');
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
     setText(e.target.value);
   };
 
@@ -21,8 +33,12 @@ function FeedbackForm() {
             placeholder='Write a review'
             value={text}
           />
-          <Button type='submit'>Send</Button>
+          <Button type='submit' isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+
+        {message && <div className='message'>{message}</div>}
       </form>
     </Card>
   );
